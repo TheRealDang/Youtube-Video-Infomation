7d61af70ed5b8301e3c5f13d76c96f77f67044c6
A Name <haidanginnhatrang@gmail.com>
2024-07-07 23:51:07 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -15,8 +15,22 @@ while True:
     yt = pytube.YouTube(url)
     sprint("Video:", yt.title,x=0,y=0)
     sprint(yt.views,"Views",x=1,y=1)
-    sprint("Author:",yt.author,x=1,y=2)
-    sprint("Publish Date:",yt.publish_date,x=1,y=3)
+    #yt.bypass_age_gate()
+    sprint(yt.initial_data['contents']
+           ['twoColumnWatchNextResults']
+           ['results']['results']
+           ['contents'][0]
+           ['videoPrimaryInfoRenderer']
+           ['videoActions']['menuRenderer']
+           ['topLevelButtons'][0]
+           ['segmentedLikeDislikeButtonViewModel']
+           ['likeButtonViewModel']['likeButtonViewModel']
+           ['toggleButtonViewModel']['toggleButtonViewModel']
+           ['defaultButtonViewModel']['buttonViewModel']
+           ['title']
+           ,"Likes",x=1,y=2)
+    sprint("Author:",yt.author,x=1,y=3)
+    sprint("Publish Date:",yt.publish_date,x=1,y=4)
     stdscr.refresh()
 
 curses.endwin()
