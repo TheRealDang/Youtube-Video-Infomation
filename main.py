f227e9bc8b0a293e09f396f25ef736595d818262
A Name <haidanginnhatrang@gmail.com>
2024-07-08 20:50:17 +0700

    Add files via upload
    slow internet

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ import curses, pytube
 stdscr = curses.initscr()
 curses.noecho()
 
+ry = 0
 
 def sprint(*string,x=0,y=0):
     try:
-        stdscr.addstr(y,x," ".join(list(map(str,string))))
+        stdscr.addstr(y-ry,x," ".join(list(map(str,string))))
     except:
       pass
 
 url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
+def get_description(yt):
+    for n in range(6):
+        try:
+            description =  yt.initial_data["engagementPanels"][n]["engagementPanelSectionListRenderer"]["content"]["structuredDescriptionContentRenderer"]["items"][1]["expandableVideoDescriptionBodyRenderer"]["attributedDescriptionBodyText"]["content"]            
+            return description
+        except:
+            continue
+    return False
+
+
 while True:
+    if '' == curses.KEY_UP:
+        if ry != 0:
+            ry -= 1
+    if '' == curses.KEY_DOWN:
+        ry += 1
     yt = pytube.YouTube(url)
     sprint("Video:", yt.title,x=0,y=0)
     sprint(yt.views,"Views",x=1,y=1)
@@ -31,6 +47,7 @@ while True:
            ,"Likes",x=1,y=2)
     sprint("Author:",yt.author,x=1,y=3)
     sprint("Publish Date:",yt.publish_date,x=1,y=4)
+    sprint("Description:\n",get_description(yt=yt),x=1,y=4)
     stdscr.refresh()
 
 curses.endwin()
